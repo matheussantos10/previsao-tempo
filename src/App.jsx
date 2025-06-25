217475bd66f76174938f0a2018f3a69f1b55d1cf
Matheus Santos <matheush.santos220@gmail.com>
2025-06-25 11:08:23 -0300

    escondendo key com gitIgnore

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import { Links } from './components/Links'
 import { WeatherSearch } from './components/WeatherSearch'
 import { WeatherForecast } from './components/WeatherForecast'
 import { WeatherInformations } from './components/WeatherInformations'
+/** API */
+import { keyAPI } from './api/key';
 
 function App() {
 
@@ -24,7 +26,7 @@ function App() {
 
     const searchCity = async () => {
         const city = inputRef.current.value
-        const key = "42bf7dc75a20866d9c4b93d35ee213a8"
+        const key = keyAPI
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
 
         const url5Days = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}&lang=pt_br&units=metric`
